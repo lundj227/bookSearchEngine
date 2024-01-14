e356a5f8b2b29da5cf27349efee008b458f21707
Jack Lund <jacklund@Jacks-MacBook-Pro.local>
2024-01-13 20:56:55 -0800

    apollo client for front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,28 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import Navbar from './components/Navbar';
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
